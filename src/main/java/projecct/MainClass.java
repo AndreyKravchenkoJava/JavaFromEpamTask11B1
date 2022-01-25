3ae6ab2d575bcf9b05ecd6a316d08c23ebe95966
Andrey Kravchenko <andreykravchenkodeveloper@gmail.com>
2022-01-25 16:36:12 +0300

    Initial commit

---
+++ src/main/java/projecct/MainClass.java
@@ -0,0 +1,31 @@
+package projecct;
+
+import java.util.ArrayList;
+
+public class MainClass {
+    public static void main(String[] args) {
+        ArrayList<Person> people = new ArrayList<>();
+        people.add(new Person(1));
+        people.add(new Person(2));
+        people.add(new Person(3));
+        people.add(new Person(4));
+        people.add(new Person(5));
+        people.add(new Person(6));
+        people.add(new Person(7));
+        people.add(new Person(8));
+        people.add(new Person(9));
+        people.add(new Person(10));
+
+        System.out.println("Circle of people: " + people);
+
+        while (people.size() > 1) {
+            for (int i = 0; i < people.size(); i++) {
+                if (i % 2 == 1) {
+                    people.remove(i);
+                }
+            }
+        }
+
+        System.out.println("Circle of people with every second person crossed out: " + people);
+    }
+}
